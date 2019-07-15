ead2d43b46063de3e17c536d12b5ad4db19eb41d
Leotis <52931974+Leotis@users.noreply.github.com>
2019-07-15 15:52:29 -0400

    Add files via upload

---
+++ attempt 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hellow world")
+print("2169" )
+print("the division of 5/2 is", 5/2)
+#create function to sum any two numbers
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)
+plus(2,3,4)
+
+#create a function to find the area of a triangle
+print(1/2*21*69)
